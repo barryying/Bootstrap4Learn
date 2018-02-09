625f0411152d944d499240be88fb441f112f1a0f
TaoYing <barryying>
2018-02-09 17:01:16 +0800

    commit

---
+++ Bootstrap4Learn/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bootstrap4Learn.Startup))]
+namespace Bootstrap4Learn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
